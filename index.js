5fa4008db8d6b95962c8277dcd25b2f6c2be7b50
Gonzalo <gonzalosantacruzariel@gmail.com>
2022-10-24 11:09:38 -0300

    agrego env y conexion a base de datos

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+//Database
+const baseDeDatos = require('./config/db.js');
 
 //Static
 app.use(express.static(__dirname + '/public'));
