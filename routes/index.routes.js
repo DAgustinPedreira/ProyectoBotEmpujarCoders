08f7755c5cbd4ba991ee480dfdc4c91fa1ea4eb2
Gonzalo <gonzalosantacruzariel@gmail.com>
2022-10-24 22:12:04 -0300

    Creo Schema y ruta al database

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,8 +2,24 @@ const express = require("express")
 const router = express.Router()
 const path = require("path")
 
+const Usuario = require("../models/Usuario");
+
+router.post("/ejemplo", async (req, res) => {
+     
+    try { const usuario = new Usuario(req.body);
+        await usuario.save();
+        res.send(usuario);
+    } catch (error) {
+       return res.send(error);
+    } 
+    
+
+});
+
+//Pagina principal
 router.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../index.html"));
-})
+ })
+
 
 module.exports = router;
