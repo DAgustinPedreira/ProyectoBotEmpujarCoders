cf633a4dd2fb663303d8888bc2492c8c6f7c289c
nicolascastrocai <nicolascastrocai@gmail.com>
2022-11-10 00:04:50 -0300

    Re diseño de la pagina principal y incorporacion de un sistema de logueo

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,54 @@
 const express = require("express")
-const router = express.Router()
+const {Router}=require('express')
+const router=Router()
 const path = require("path")
+const passport=require('passport')
+require('../passport/local-passport')
+const User=require('../models/usuarios')
+
+
+
+
 
 router.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../index.html"));
 })
 
+router.get('/registro',(req,res)=>{
+    res.render('register.ejs')
+  })
+
+  router.post('/registro',passport.authenticate('local-/register',{
+    successRedirect:'/login',
+    failureRedirect:'/registro',
+    passReqToCallback:true
+}))
+
+  router.get('/login',(req,res)=>{
+    res.render('login.ejs')
+  })
+
+  router.post('/login',passport.authenticate('local-/login',{
+    successRedirect:'/bot',
+    failureRedirect:'/login',
+    passReqToCallback:true
+}))
+
+router.get('/bot',isAuthenticated,(req,res)=>{
+  res.render('bot.ejs')
+})
+
+
+
+
+function isAuthenticated(req,res,next) {
+  if (req.isAuthenticated()) {
+      return next()
+  }
+  res.redirect('login')
+}
+
+
+
 module.exports = router;
+
