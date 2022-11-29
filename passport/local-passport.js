95595fa234b28bb2131a82ff4b43ecb3e6bc8af9
nicolascastrocai <nicolascastrocai@gmail.com>
2022-11-29 20:19:10 -0300

    Mejora del Login

--- passport/local-passport.js
+++ passport/local-passport.js
@@ -2,19 +2,18 @@ const { model } = require('mongoose')
 const passport=require('passport')
 const usuarios = require('../models/usuarios')
 const LocalStrategy=require('passport-local').Strategy
+const bcrypt=require('bcryptjs')
 
 const User=require('../models/usuarios')
 
-passport.serializeUser((usuario,done)=>{
-    done(null,usuario.id)
-})
+const encrypt= async (textPlain) =>{
+    const hash=await bcrypt.hash(textPlain,10)
+    return hash
+}
 
-passport.deserializeUser((id,done)=>{
-    User.findById(id)
-        .then(usuarios=>{
-            done(null,usuarios)
-        })
-})
+const compare= async (contraseña,hashContraseña) =>{
+    return await bcrypt.compare(contraseña,hashContraseña)
+}
 
 
 
@@ -22,15 +21,17 @@ passport.use('local-/register',new LocalStrategy({
     usernameField:'usuario',
     passwordField:'contraseña',
     passReqToCallback:true
-},(req,usuario,contraseña,done)=>{
+},async (req,usuario,contraseña,done)=>{
+     let paswordHash=await encrypt(contraseña)
      const user=User.findOne({usuario:usuario},(err,user)=>{
+        
         if (user) {
             done(null,false,req.flash('mensaje', 'El Usuario ya esta registrado'))
         }
         else{
             const usuarios=new User()
             usuarios.usuario=usuario
-            usuarios.contraseña=contraseña
+            usuarios.contraseña=paswordHash
             usuarios.save()
                 .then(usuario=>{
                     done(null,usuario)
@@ -44,22 +45,25 @@ passport.use('local-/login',new LocalStrategy({
     usernameField:'usuario',
     passwordField:'contraseña',
     passReqToCallback:true
-},(req,usuarios,contraseña,done)=>{
-    const user=User.findOne({usuario:usuarios},(err,user)=>{
+},async(req,usuarios,contraseña,done)=>{  
+
+    const user= User.findOne({usuario:usuarios},async (err,user)=>{
+        let checkPassword=await compare(contraseña,user.contraseña)
+        
         if (!user) {
             done(null,false,req.flash('mensaje2', 'El Usuario no esta registrado'))
         }
         else{
-            User.findOne({contraseña:contraseña},(err,go)=>{
-                if (!go) {
+            
+                if (!checkPassword) {
                     done(null,false,req.flash('mensaje3', 'contraseña incorrecta '))
                 }
 
-                else if(go){
+                else if(checkPassword){
                     
                     done(null,user)
                 }
-            })
+           
         }
     })
 }))
