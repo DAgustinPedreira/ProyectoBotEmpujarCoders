d3274c80ba67cc9237fcea50ea677c9f4f708cce
Gonzalo <gonzalosantacruzariel@gmail.com>
2023-05-19 16:05:03 -0300

    sigo intentando

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
 })
 
 router.get('/registro',(req,res)=>{
-    res.render('../views/register.ejs')
+    res.render('./views/register.ejs')
   })
 
   router.post('/registro',passport.authenticate('local-/register',{
