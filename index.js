c70b0a4b4b2d75312a9ea49e229f8f6d88192281
Gonzalo <gonzalosantacruzariel@gmail.com>
2024-03-15 02:13:52 -0300

    rutas

--- index.js
+++ index.js
@@ -2,62 +2,52 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-const passport=require('passport')
-const session = require('express-session')
-const flash=require('connect-flash')
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('connect-flash');
 require("./config/passport");
 
-
-
-//cambiado de 3000 a 27017
-
-
-
-
-
-
-
-//Database
+// Database
 require('./config/db.js');
 
-
-//Mi
-app.use(express.urlencoded({extended:false}))
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
 app.use(session({
-  secret:'nicolas',
-  resave:false,
-  saveUninitialized:false
-}))
+  secret: 'nicolas',
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  app.locals.mensaje = req.flash('mensaje');
+  app.locals.mensaje2 = req.flash('mensaje2');
+  app.locals.mensaje3 = req.flash('mensaje3');
+  app.locals.user = req.user || null;
+  next();
+});
 
-app.use((req,res,next)=>{
-  app.locals.mensaje=req.flash('mensaje')
-  app.locals.mensaje2=req.flash('mensaje2')
-  app.locals.mensaje3=req.flash('mensaje3')
-  app.locals.user=req.user||null
-  next()
-})
+// Routes
+const routes = require("./routes/index.routes");
+app.use(routes);
 
-
-//Rutas
-
-const routes = require("./routes/index.routes")
-app.use(routes)
-
-//Static
-app.use(express.static(__dirname + '/public'));
-// app.use(express.static('views'))
+// Static
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.set("views", "public/views");
 
+// Home Route
+app.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("index");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-
-
-
-//Servidor
+// Server
 app.listen(port, () => {
   console.log(`Servidor en puerto ${port}`);
 });
-
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,51 +1,44 @@
-const {Router}=require('express')
-const router=Router()
-const path = require("path")
-const passport=require('passport')
-require('../passport/local-passport')
-const User=require('../models/usuarios')
-
-
+const { Router } = require('express');
+const router = Router();
+const path = require("path");
+const passport = require('passport');
+require('../passport/local-passport');
+const User = require('../models/usuarios');
 
 router.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../index.html"));
-})
+    res.render("index.ejs");
+});
 
-router.get('/registro',(req,res)=>{
-    res.render('register.ejs')
-  })
+router.get('/registro', (req, res) => {
+    res.render('register.ejs');
+});
 
-  router.post('/registro',passport.authenticate('local-/register',{
-    successRedirect:'login',
-    failureRedirect:'registro',
-    passReqToCallback:true
-}))
+router.post('/registro', passport.authenticate('local-/register', {
+    successRedirect: '/login',
+    failureRedirect: '/registro',
+    passReqToCallback: true
+}));
 
-  router.get('/login',(req,res)=>{
-    res.render('login.ejs')
-  })
+router.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
 
-  router.post('/login',passport.authenticate('local-/login',{
-    successRedirect:'bot',
-    failureRedirect:'login',
-    passReqToCallback:true
-}))
+router.post('/login', passport.authenticate('local-/login', {
+    successRedirect: '/bot',
+    failureRedirect: '/login',
+    passReqToCallback: true
+}));
 
-router.get('/bot',isAuthenticated,(req,res)=>{
-  res.render('bot.ejs')
-})
+router.get('/bot', isAuthenticated, (req, res) => {
+    res.render('bot.ejs');
+});
 
-
-
-
-function isAuthenticated(req,res,next) {
-  if (req.isAuthenticated()) {
-      return next()
-  }
-  res.redirect('login')
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
 }
-
-
 
 module.exports = router;
 
